73bee05c5b83a4ba484caca19378b7404db87ea0
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-11-09 12:52:46 +0100

    send and show data

--- MyChat/chatarea/admin.py
+++ MyChat/chatarea/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Message, Chat
+
+# Antatt alle Felder anzeigen zu lassen, kann man auch Auswahl treffen
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('chat','text','created_at', 'author', 'receiver')
+    # sehe nur noch diese Infos im Interface
+    list_display = ('created_at', 'author', 'text', 'receiver')
+    # zeigt Infos schon in Vorauswahl
+    search_fields = ('text',)
+    #kann Nachrichten nach Text filtern
 
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- MyChat/chatarea/views.py
+++ MyChat/chatarea/views.py
@@ -1,6 +1,31 @@
+from django.http import HttpResponseRedirect, JsonResponse
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from .models import Message
+from .models import Chat
+from django.contrib.auth.decorators import login_required
+from django.core import serializers
 
 # Create your views here.
 
+
+@login_required(login_url='/login/')
 def index(request):
-    return render(request, 'chat/index.html')
+    """
+    this is a view to render the chat html
+    """
+    if request.method == 'POST':
+        print("Received Data " + request.POST['textmessage'])
+        testChat = Chat.objects.get(id=1)
+        # testChat ist jetzt immer Chat mit id=1 (manuell angelegt) --> später dynamisieren
+        new_message = Message.objects.create(text=request.POST['textmessage'], chat=testChat, author=request.user, receiver=request.user)
+        # Input von Chat-Seite wird in Datenbank als neues Objekt gespeichert
+        serialized_obj = serializers.serialize('json', [new_message])
+        # wandeln erstelltes Objekt in JSON um
+        return JsonResponse(serialized_obj[1:-1], safe=False)
+        # response ist String in Array --> nehmen dessen Substring, den wir umwandeln
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages': chatMessages})
+    # der Pfad reicht aus, da immer automatisch im Templat Ordner nachgesehen wird
+    # können der Funktion ein JSON-Objekt mitgeben --> Variable hardcode oder dynamisch, z.B. die Messages aus Chat 1
